95c6fc9b60afbd5efbe16d8fe73cacaf33b0e46d
paulaanb <paulanb2000@gmail.com>
2021-11-21 12:38:15 +0100

    Espero que este código funcione

--- SimpleBlackJack.py
+++ SimpleBlackJack.py
@@ -21,6 +21,64 @@ cartas = {
 #Base del juego
 def game():
     print("Cartas: {}".format(" ".join(cartas.keys())))
-    print("Puntos: {}".format(list(cartas.values())))
-    
+    print("Puntos: {}".format(list(cartas.values())))       
 #Establecemos el valor de cada carta
+    print("1\ Iteración estándar sobre un diccionario")
+    for carta, valor in cartas.items():
+        print("la carta {} vale {}".format(carta, valor))
+ 
+    print("2\ Iteración ordenada sobre un diccionario")
+    for carta in sorted(cartas.keys()):
+        print("la carta {} vale {}".format(carta, cartas[carta]))
+
+#Le adjuntamos una lista al dicctionario de cartas
+    print("3\ Black Jack")
+    lista_cartas = list(cartas)
+    
+#Ahora nos disponemos a repartir las dos cartas al jugador y calcular su valor total
+    print("Usted ha seleccionado la carta:", end=" ")
+    carta = choice(lista_cartas)
+    score = cartas[carta]
+    print(carta, end=" ")
+    carta = choice(lista_cartas)
+    score += cartas[carta]
+    print(carta, end=" ")
+    print(" \n-> Su puntuación es de", score)
+
+#Si el valor total de las anteriores cartas es menor a 21, podra seguir jugando
+    stick= False
+    while score < 21 and stick ==False:
+        new_card = input("¿Quiere solicitar una carta nueva? 1) Si 2) No:")
+        new_card = new_card.lower()
+        if new_card == "1":
+            carta = choice(lista_cartas)
+            score += cartas[carta]
+            print(carta, end=" ")
+            print("\n-> Su puntuación es de", score)
+        elif new_card == "2":
+            stick= True
+        else:
+            print("Por favor, elija una respuesta permitida")
+#Ahora es el turno de la banca
+main_banca = sample(lista_cartas, 2)
+score_banca = sum(cartas[carta] for carta in main_banca)
+print("La banca tiene: {} {}  >> su score es {}".format(main_banca[0], main_banca[1], score_banca))
+
+#La puntuación de mi juego depende del valor de las cartas repartidas.
+if score > 21:
+    print("Lo lamento, su valor es mayor de 21, el ganandor ha sido la banca.")
+if score == score_banca:
+    print("Ha habido un empate entre usted y la banca.")
+if score < 21 and score < score_banca:
+    print("El ganador fue la banca.")
+if score < 21 and score > score_banca:
+    print("¡Enhorabuena! Consiguió realiza un Blackjack, ha ganado la partida.")
+play_again= input("¿Desea volver a jugar? 1)Si 2)No")
+play_again= play_again.lower()
+if play_again == "1":
+    play()
+if play_again == "2":
+    print("¡Nos vemos pronto!")
+
+#Creamos la función para generar el juego
+play()
