e7ec19f0f9d322e931e111d6ad2d4408045d5643
paulaanb <paulanb2000@gmail.com>
2021-11-21 12:13:44 +0100

    cambios para un programa mas sencillo

--- SimpleBlackJack.py
+++ SimpleBlackJack.py
@@ -16,36 +16,11 @@ cartas = {
     chr(0x1f0ab): 10,
     chr(0x1f0ad): 10,
     chr(0x1f0ae): 10,
+}
 
 #Base del juego
-class BlackJack():
-    def __init__(self):
-        self.turn = []
-        self.deckofcards = list(cards.key())
-        self.dealer = []
-    def giveCards(self, amount, bunch):
-        for i in range(amount):
-            shuffle(self.deckofcards)
-            card = self.deckofcards.pop()
-            bunch.append(card)
-    def calculate(self, bunch):
-        score = 0
-        for i in range(len(bunch)):
-            try:
-                score += card[bunch[i]]
-            except: 
-                return print("Ha habido un error en el código")
-        return score
-    def begingame(self):
-        print("¡Vamos a empezar a jugar al BlackJack! \n El dealer se dispone a darle sus cartas.")
-        blackjack().giveCards(2, self.hand)
-        blackjack().giveCards(1, self.dealer)
-        print("Su mano en esta partida es:\n" + self.hand[0] + '' + self.hand[1] + "\nScore:" + str(blackjack().calculatehand(self.hand)) + "\nDealer:" + self.dealer[0] + "\nScore" + str(blackjack().calculatehand)))
-        decision = input("Ahora debe elegir lo que desea hacer, si continuar la partida o plantarse (Stick/Continue):")
-        if str(decision).lower()== "stick":
-            blackjack().stick(self.hand)
-        elif str(decision).lower() == "continue":
-            blackjack().continue()
-    def stick(self, bunch):
-        shuffle(self.deckofcards)
-        card= 
+def game():
+    print("Cartas: {}".format(" ".join(cartas.keys())))
+    print("Puntos: {}".format(list(cartas.values())))
+    
+#Establecemos el valor de cada carta
